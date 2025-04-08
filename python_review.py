a704af5efe7ffea672eb2c552eacb6f42127ee47
CSHan-Berklee <chan6@berklee.edu>
2025-04-08 19:55:06 -0400

    Level 7 Complete

--- python_review.py
+++ python_review.py
@@ -1,3 +1,30 @@
-x = type(3)
-y = type("Hello")
-z = type(True)
+my_integer = 42 #int
+my_integer_type = type(my_integer)
+print(my_integer_type)
+my_float = 3.14  #float
+my_float_type = type(my_float)    
+print(my_float_type)
+my_string = "Hello, World!" #strings
+my_string_type = type(my_string)
+print(my_string_type)
+my_boolean_True = True #bool
+my_boolean_True_type = type(my_boolean_True)
+print(my_boolean_True_type)
+my_boolean_False = False #bool
+my_boolean_False_type = type(my_boolean_False)
+print(my_boolean_False_type)
+my_none_value = None #Nonetype
+my_none_value_type = type(my_none_value)
+print(my_none_value_type)
+my_negative_integer = -7 #int
+my_negative_integer_type = type(my_negative_integer)
+print(my_negative_integer_type) 
+my_large_float = 1.23456789e10  #float
+my_large_float_type = type(my_large_float)
+print(my_large_float_type)
+my_single_character = 'A'   #string
+my_single_character_type = type(my_single_character)
+print(my_single_character_type)
+my_empty_string = ""  #string
+my_empty_string_type = type(my_empty_string)
+print(my_empty_string_type)
