7b6f5b8e1408b46e2e2f545b0f8155bf4841341c
CSHan-Berklee <chan6@berklee.edu>
2025-04-08 19:31:10 -0400

    Level 6 Complete

--- python_review.py
+++ python_review.py
@@ -1,10 +1,3 @@
-'''
-A variable name must start with a letter or an underscore
-A variable name cannot start with a number
-A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _)
-Variable names are case sensitive (AGE age Age are three variables)
-a variable name cannot be any of the python keywords
-
-
-$$$Variable = "Bad"
-'''
+x = type(3)
+y = type("Hello")
+z = type(True)
